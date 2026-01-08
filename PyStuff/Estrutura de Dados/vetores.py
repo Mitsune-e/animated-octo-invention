d87f3e9438d19fcbb676228bcb6a1694f395d63c
Mitsune-e <gvelosodev@gmail.com>
2026-01-08 17:24:26 -0300

    Estudo vetores 1

--- PyStuff/Estrutura de Dados/vetores.py
+++ PyStuff/Estrutura de Dados/vetores.py
@@ -6,5 +6,13 @@ class Vetor():
     def inserir_elemento_posicao(self, elemento, posicao):
         self.__elementos[posicao] = elemento
 
+    def inserir_elemento_final(self, elemento):
+        for i in range(self.__tamanho):
+            if self.__elementos[i] is None:
+                self.__elementos[i] = elemento
+                return
+        raise Exception("Vetor cheio")
+    
+    
     def listar_elemento(self, posicao):
         return self.__elementos[posicao]
