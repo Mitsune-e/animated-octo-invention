9c8012515aeaaa174bacd191fc5af766c842d5ee
Mitsune_e <gvelosodev@gmail.com>
2026-04-22 15:42:30 -0300

    Godot Signals

--- godot/godotfun/GodotCS/Scenes/Wizard/Wizard.cs
+++ godot/godotfun/GodotCS/Scenes/Wizard/Wizard.cs
@@ -3,13 +3,22 @@ using System;
 
 public partial class Wizard : Node2D
 {
+	[Export] private Timer _timer;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Hide();
+		_timer.Timeout += Appear;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
+	}
+
+	private void Appear()
+	{
+		Show();
 	}
 }
